cb4009f68ff36f0c5a15622fbb8bf9814c45f329
Matt Felsen <mattfelsen@gmail.com>
2016-06-21 00:46:51 -0400

    Create basic example project files for demoing different cam types

--- example-basic/src/ofApp.cpp
+++ example-basic/src/ofApp.cpp
@@ -3,7 +3,33 @@
 //--------------------------------------------------------------
 void ofApp::setup() {
 	// Set the type of camera for this specific instance
-	device.setup<OrbbecAstra>();
+
+	// If using a Kinect v1 with ofxKinect, uncomment the next line, and
+	// uncomment `#define OFX_DEPTH_CAMERA_KINECT` in ofxDepthCamera.h
+//	device.setup<Kinect>();
+
+	// If using a Kinect v2 with ofxKinectV2, uncomment the next line, and
+	// uncomment `#define OFX_DEPTH_CAMERA_KINECT_V2` in ofxDepthCamera.h
+//	device.setup<KinectV2>();
+
+	// If using a Kinect v2 with ofxMultiKinectV2, uncomment the next line, and
+	// uncomment `#define OFX_DEPTH_CAMERA_MULTI_KINECT_V2` in ofxDepthCamera.h
+//	device.setup<MultiKinectV2>(true);
+
+	// If using a Kinect v2 with ofxKinectForWindows2, uncomment the next line, and
+	// uncomment `#define OFX_DEPTH_CAMERA_KFW2` in ofxDepthCamera.h
+//	device.setup<KinectForWindows2>();
+
+	// If using an Orbbec Astra with ofxOrbbecAstra, uncomment the next line, and
+	// uncomment `#define OFX_DEPTH_CAMERA_ORBBEC_ASTRA` in ofxDepthCamera.h
+//	device.setup<OrbbecAstra>(true);
+
+	// If you have an Orbbec Astra Pro and are seeing a blank screen, try changing
+	// true in setup above to false and uncommenting the following lines
+//	auto ptr = static_pointer_cast<OrbbecAstra>(device.getPointer());
+//	ptr->getSensor().initVideoGrabber();
+
+	// Set the near/far depth values in millimeters
 	device.setDepthClipping(400, 4500);
 
 	// Defaults to live, directly connected device. You can manually set with:
@@ -27,7 +53,7 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 	device.getDepthImage().draw(0, 0);
-    device.getColorImage().draw(0, device.getDepthHeight());
+    device.getColorImage().draw(0, device.getDepthHeight(), device.getDepthWidth(), device.getDepthHeight());
 }
 
 //--------------------------------------------------------------
